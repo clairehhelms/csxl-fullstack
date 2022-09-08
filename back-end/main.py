5388a96ab099e73cca9640cbf7a285d4cab5940a
Jesse Wei <jessew13@email.unc.edu>
2022-09-08 18:18:37 -0400

    bruh meme syntax errors in backend

--- back-end/main.py
+++ back-end/main.py
@@ -1,8 +1,9 @@
-# from fastapi import FastAPI, Depends
-# from database import Base, engine, get_db
-# from schemas import Link
-# from models import LinkCreate
+from fastapi import FastAPI, Depends
+from database import Base, engine, get_db
+from schemas import Link
+from models import LinkCreate
 
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # python -m uvicorn main:app --reload
@@ -22,14 +23,14 @@ links: list[Link] = []
 
 
 @app.post("/api/link")
-def create_link(display_name: str, url: str) -> Link:
-    link = Link()
-    link.display_name = display_name
-    link.url = url
-    links.append(link)
-    # return "test"
+def create_link(link: LinkCreate, db = Depends(get_db)):
+    db_link = Link(display_name = link.display_name, url = link.url)
+    db.add(db_link)
+    db.commit()
+    db.refresh(db_link)
+    return db_link
 
 
 @app.get("/api/links")
-def get_links() -> list[Link]:
-    return links
+def get_links(db = Depends(get_db)):
+    return db.query(Link).all()
