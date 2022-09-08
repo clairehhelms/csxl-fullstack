15407770381f427cd8e4c8e09e0d39b18754777c
Jesse Wei <jessew13@email.unc.edu>
2022-09-08 17:39:14 -0400

    delete code after this commit

--- back-end/main.py
+++ back-end/main.py
@@ -2,6 +2,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# python -m uvicorn main:app --reload
+
+
 @app.get("/api/health")
 def read_root():
     return "Hello World!"
+
+
+class Link:
+    display_name: str
+    url: str
+
+
+links: list[Link] = []
+
+
+@app.post("/api/link")
+def create_link(display_name: str, url: str) -> Link:
+    link = Link()
+    link.display_name = display_name
+    link.url = url
+    links.append(link)
+    return link
+
+
+@app.get("/api/links")
+def get_links() -> list[Link]:
+    return links
